e760a525268785d7010dd3d6743405c5e15444ad
WacArteec <art.kaznad@gmail.com>
2024-09-25 16:24:59 +0300

    all corrections (guess, final)

--- src/Sorter.cpp
+++ src/Sorter.cpp
@@ -6,7 +6,7 @@
 #include "Sorter.h"
 #include <StructuresForOnegin.h>
 
-int StrCmp(char* str1, char* str2);
+int StrCmp(struct Lines str1, struct Lines str2);
 void AddrSwapper(void **a, void **b);
 int CmpAndCleanBuff(const char refer);
 int ReverseStrCmp(struct Lines str1, struct Lines str2);
@@ -19,46 +19,38 @@ void Sorter(struct Book* Onegin)
 $$$ assert(Onegin);
 
     printf("Enter r to reverse sort or another button to common sort \n");
-    if(CmpAndCleanBuff('r'))
-        for(size_t i = 0; i < Onegin->count_lines; i++)
+    int (*Comparator)(struct Lines, struct Lines);
+    if(CmpAndCleanBuff('r'))    Comparator = ReverseStrCmp;
+    else                        Comparator = StrCmp;
+
+    for(size_t i = 0; i < Onegin->count_lines; i++)
+    {
+        for(size_t j = 0; j < Onegin->count_lines - 1 - i; j++)
         {
-            for(size_t j = 0; j < Onegin->count_lines - 1 - i; j++)
+            if(Comparator(Onegin->str[j], Onegin->str[j+1]) > 0)
             {
-                if(ReverseStrCmp(Onegin->str[j], Onegin->str[j+1]) > 0)
-                {
-                    AddrSwapper((void**)&(Onegin->str[j].adr), (void**)&(Onegin->str[j+1].adr));
-                }
+                AddrSwapper((void**)&(Onegin->str[j].adr), (void**)&(Onegin->str[j+1].adr));
             }
         }
-    else
-        for(size_t i = 0; i < Onegin->count_lines; i++)
-        {
-            for(size_t j = 0; j < Onegin->count_lines - 1 - i; j++)
-            {
-                if(StrCmp(Onegin->str[j].adr, Onegin->str[j+1].adr) > 0)
-                {
-                    AddrSwapper((void**)&(Onegin->str[j].adr), (void**)&(Onegin->str[j+1].adr));
-                }
-            }
-        }
+    }
 }
 
-int StrCmp(char* str1, char* str2)
+int StrCmp(struct Lines str1, struct Lines str2)
 {
     int i = 0, j = 0;
-    printf("COMPARE_ENTER %s %s\n", str1,str2);
+//$$$    printf("COMPARE_ENTER %s %s\n", str1,str2);
 //$$$     printf("%d \n", __LINE__); //!!txlib!! $$ $meow(hello)
 
-        while (isalpha(str1[i]) == 0 && str1[i] != '\0')     i++;
-        while (isalpha(str2[j]) == 0 && str2[j] != '\0')     j++;
+        while (isalpha(str1.adr[i]) == 0 && str1.adr[i] != '\0')     i++;
+        while (isalpha(str2.adr[j]) == 0 && str2.adr[j] != '\0')     j++;
 
-        while (str1[i] == str2[j] && str1[i] != '\0' && str2[j] != '\0')
+        while (str1.adr[i] == str2.adr[j] && str1.adr[i] != '\0' && str2.adr[j] != '\0')
         {
             i++;
             j++;
         }
-        printf("COMAPRE %s %s res=%d\n", str1, str2, str1[i] - str2[j]);
-        return (str1[i] - str2[j]);
+        printf("COMAPRE %s %s res=%d\n", str1.adr, str2.adr, str1.adr[i] - str2.adr[j]);
+        return (str1.adr[i] - str2.adr[j]);
 
     return 0;
 }
